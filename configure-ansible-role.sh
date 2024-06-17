4f3cba937be0c8d960b1cd6ea4817bb2baa09977
Tosin Akinosho <tosin.akinosho@gmail.com>
2024-06-17 19:04:11 -0400

    chore: Update configure-ansible-role.sh to set hostname and perform system updates

--- configure-ansible-role.sh
+++ configure-ansible-role.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Check if a hostname is provided
 if [ -z "$1" ]; then
     echo "Please provide the name of the host to configure."
     exit 1
 fi
-
 
 # Define variables
 export DEBIAN_FRONTEND=noninteractive
@@ -13,43 +13,72 @@ ROLE_REPO="https://github.com/tosin2013/code-server-instance.git"
 INVENTORY_FILE="inventory"
 PLAYBOOK_FILE="playbook.yml"
 
+# Logging function
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
 # Check if the hostname is already set to the provided value
 CURRENT_HOSTNAME=$(hostnamectl status --static)
-
 if [ "$CURRENT_HOSTNAME" != "$1" ]; then
-    hostnamectl set-hostname "$1"
-    echo "Hostname has been set to $1"
+    sudo hostnamectl set-hostname "$1"
+    if [ $? -eq 0 ]; then
+        log "Hostname has been set to $1"
+    else
+        log "Failed to set hostname to $1"
+        exit 1
+    fi
 else
-    echo "Hostname is already set to $1"
+    log "Hostname is already set to $1"
 fi
 
 # Update and upgrade the system
-sudo apt update && sudo apt upgrade -y
+log "Updating and upgrading the system"
+sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+if [ $? -ne 0 ]; then
+    log "Failed to update and upgrade the system"
+    exit 1
+fi
 
 # Install Ansible if it is not already installed
-if ! command -v ansible &> /dev/null
-then
-    sudo apt install -y ansible
+if ! command -v ansible &> /dev/null; then
+    log "Installing Ansible"
+    sudo apt-get install -y ansible -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+    if [ $? -ne 0 ]; then
+        log "Failed to install Ansible"
+        exit 1
+    fi
+else
+    log "Ansible is already installed"
 fi
 
 # Create Ansible roles directory if it doesn't exist
 mkdir -p ~/ansible/roles
 
 # Change to the Ansible roles directory
-cd ~/ansible/roles
+cd ~/ansible/roles || exit
 
 # Clone the Ansible role repository
 if [ ! -d "$ROLE_NAME" ]; then
+    log "Cloning Ansible role repository"
     git clone $ROLE_REPO $ROLE_NAME
+    if [ $? -ne 0 ]; then
+        log "Failed to clone Ansible role repository"
+        exit 1
+    fi
+else
+    log "Ansible role repository already exists"
 fi
 
 # Create inventory file
+log "Creating inventory file"
 cat <<EOF > ~/ansible/$INVENTORY_FILE
 [all]
 localhost ansible_connection=local
 EOF
 
 # Create playbook file
+log "Creating playbook file"
 cat <<EOF > ~/ansible/$PLAYBOOK_FILE
 ---
 - hosts: all
@@ -60,9 +89,14 @@ cat <<EOF > ~/ansible/$PLAYBOOK_FILE
 EOF
 
 # Change to the Ansible directory
-cd ~/ansible
+cd ~/ansible || exit
 
 # Run the playbook
+log "Running the Ansible playbook"
 ansible-playbook -i $INVENTORY_FILE $PLAYBOOK_FILE
+if [ $? -ne 0 ]; then
+    log "Failed to run Ansible playbook"
+    exit 1
+fi
 
-echo "Ansible role $ROLE_NAME has been configured and executed."
+log "Ansible role $ROLE_NAME has been configured and executed successfully."
